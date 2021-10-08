67ce9b49d3ebd2ccb529ad354b769cf0c2ab02c9
joshliuzh <31194471+joshliuzh@users.noreply.github.com>
2021-10-08 15:24:09 -0400

    Update binarySearch.py

--- binarySearch.py
+++ binarySearch.py
@@ -6,7 +6,8 @@ Created on Sat Jul 17 15:45:57 2021
 @author: Josh Liu
 """
 
-def binSearch1(A, t): 
+
+def searchLow(nums, target):
     """
     Binary search that returns the minimum index i, such that A[i] >= target. If no such element exists, return N, the length of A.
     Input: A, a list of elements to be searched. MUST BE sorted in ascending order.
@@ -14,29 +15,29 @@ def binSearch1(A, t):
             
     Returns: an integer
     """
-    
-    
-    l, r = 0, len(A)
-    while l < r:
-        m = (l + r) // 2 # difference (1)
-        if A[m] >= t:
-            r = m
+    lo, hi = 0, len(nums) - 1
+    while lo <= hi:
+        mid = (lo + hi)//2
+        if nums[mid] >= target:
+            hi = mid - 1
         else:
-            l = m + 1
-    return l
+            lo = mid + 1
+    return lo
+
 
 
 # Now let's try some examples:
 A = [0, 1, 1, 2]
-binSearch1(A, -1) # returns 0
-binSearch1(A, 0) # returns 0
-binSearch1(A, .5) # returns 1
-binSearch1(A, 1) # returns 1
-binSearch1(A, 1.5) # returns 3
-binSearch1(A, 2) # returns 3
-binSearch1(A, 2.1) # returns 4
+searchLow(A, -1) # returns 0
+searchLow(A, 0) # returns 0
+searchLow(A, .5) # returns 1
+searchLow(A, 1) # returns 1
+searchLow(A, 1.5) # returns 3
+searchLow(A, 2) # returns 3
+searchLow(A, 2.1) # returns 4
 
-def binSearch2(A, t):
+
+def searchHigh(nums, target):
     """
     Binary search that returns the maximum index i, such that A[i] <= target. If no such element exists, return -1.
     Input: A, a list of elements to be searched. MUST BE sorted in ascending order.
@@ -45,22 +46,23 @@ def binSearch2(A, t):
     Returns: an integer
     """
     
-  
-    l, r = -1, len(A) - 1
-    while l < r:
-        m = (l + r + 1) // 2  # difference (1)
-        if A[m] <= t:
-            l = m
+    lo, hi = 0, len(nums) - 1
+    while lo <= hi:
+        mid = (lo + hi)//2
+        if nums[mid] > target:
+            hi = mid - 1
         else:
-            r = m - 1
-    return r
+            lo = mid + 1
+    return hi
+    
+
 
 # Again, let's try some examples:
 A = [0, 1, 1, 2]
-binSearch2(A, -1) # returns -1
-binSearch2(A, 0) # returns 0
-binSearch2(A, .5) # returns 0
-binSearch2(A, 1) # returns 2
-binSearch2(A, 1.5) # returns 2
-binSearch2(A, 2) # returns 3
-binSearch2(A, 2.1) # returns 3
+searchHigh(A, -1) # returns -1
+searchHigh(A, 0) # returns 0
+searchHigh(A, .5) # returns 0
+searchHigh(A, 1) # returns 2
+searchHigh(A, 1.5) # returns 2
+searchHigh(A, 2) # returns 3
+searchHigh(A, 2.1) # returns 3
