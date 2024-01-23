43a29b3e4de92ef521044ec7d6cbfc2615c2516a
joshliuzh <31194471+joshliuzh@users.noreply.github.com>
2024-01-22 20:31:54 -0500

    Update and rename unionFind to unionFind.py

--- unionFind.py
+++ unionFind.py
@@ -7,7 +7,8 @@ Created on Tue Jul 27 21:15:57 2021
 """
 
 parent = list(range(n))
-        
+rank = [0] * n
+
 def find(x):
     nonlocal parent
     if x != parent[x]:
@@ -15,7 +16,10 @@ def find(x):
     return parent[x]
 
 def union(x, y):
+
     nonlocal parent
+    if xr == yr:
+
     rx, ry = find(x), find(y)
         if rx != ry:
     parent[rx] = ry
