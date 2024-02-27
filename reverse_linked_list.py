d63a72bf4571f0514291231cb1ed99f70f572836
joshliuzh <31194471+joshliuzh@users.noreply.github.com>
2024-02-26 21:21:54 -0500

    Create reverse_linked_list.py

---
+++ reverse_linked_list.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverse_list(self, head):
+        previous = None
+        current = head
+        while current is not None:
+            next_node = current.next
+            current.next = previous
+            previous = current
+            current = next_node
+        return previous  
