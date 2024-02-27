708ddb15429f5b3bf3dc6dd8809e1368623a0140
joshliuzh <31194471+joshliuzh@users.noreply.github.com>
2024-02-26 21:16:27 -0500

    Create 2nd_half_of_linked_list.py

---
+++ 2nd_half_of_linked_list.py
@@ -0,0 +1,8 @@
+class Solution:
+  def end_of_first_half(self, head):
+        fast = head
+        slow = head
+        while fast.next is not None and fast.next.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
