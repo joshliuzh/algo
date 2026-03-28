0113f423a92a03781f1598a93399783ed03869c9
joshliuzh <31194471+joshliuzh@users.noreply.github.com>
2026-03-28 10:39:16 -0400

    Refactor union-find implementation into a class
    Refactor union-find functions into a UnionFind class with path compression and union by size.

--- unionFind.py
+++ unionFind.py
@@ -6,27 +6,30 @@ Created on Tue Jul 27 21:15:57 2021
 @author: liuzhao
 """
 
-parent = list(range(n))
-rank = [0] * n
+class UnionFind:
+    def __init__(self, n):
+        # Initially, each element is its own parent
+        self.parent = list(range(n))
+        # Initially, each set has a size of 1
+        self.size = [1] * n
 
-def find(x):
-    nonlocal parent
-    if x != parent[x]:
-        parent[x] = find(parent[x])
-    return parent[x]
+    def find(self, x):
+        # Path compression: make nodes point directly to the root
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
 
-def union(x, y):
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
 
-    nonlocal parent, rank
-
-    xr, yr = find(x), find(y)
-    if xr == yr:
-        return
-    rankx, ranky = rank[x], rank[y]
-    if rankx < ranky:
-        parent[xr] = yr
-
-    else:
-        parent[yr] = xr
-        rank[xr] += rankx == ranky
-        
+        if root_x != root_y:
+            # Union by Size: attach the smaller tree to the larger tree
+            if self.size[root_x] < self.size[root_y]:
+                self.parent[root_x] = root_y
+                self.size[root_y] += self.size[root_x]
+            else:
+                self.parent[root_y] = root_x
+                self.size[root_x] += self.size[root_y]
+            return True # Successfully merged
+        return False # Already in the same set
