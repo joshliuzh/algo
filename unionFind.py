ee16d73f09795dfbdb323fd58446eef6ecb2543b
joshliuzh <31194471+joshliuzh@users.noreply.github.com>
2026-05-20 10:44:14 -0400

    Update unionFind.py

--- unionFind.py
+++ unionFind.py
@@ -8,35 +8,26 @@ Created on Tue Jul 27 21:15:57 2021
 
 class UnionFind:
     def __init__(self, n):
-        # Initially, each element is its own parent
         self.parent = list(range(n))
-        # Initially, each set has a size of 1
-        self.size = [1] * n
+        self.rank = [0] * n  # Tracks tree height approximation
 
     def find(self, x):
-        # Path compression: make nodes point directly to the root
         if self.parent[x] != x:
             self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
 
     def union(self, x, y):
-        root_x = self.find(x)
-        root_y = self.find(y)
-
-        if root_x != root_y:
-            # Union by Size: attach the smaller tree to the larger tree
-            if self.size[root_x] < self.size[root_y]:
-                self.parent[root_x] = root_y
-                self.size[root_y] += self.size[root_x]
-            else:
-                self.parent[root_y] = root_x
-                self.size[root_x] += self.size[root_y]
-            return True # Successfully merged
-        return False # Already in the same set
-
-    def get_set_size(self, x):
-        """Returns the total number of elements in the set containing x."""
-        root = self.find(x)
-        return self.size[root]
-
-
+        root_x, root_y = self.find(x), self.find(y)
+        if root_x == root_y:
+            return False
+        
+        # Attach the shorter tree to the taller tree
+        if self.rank[root_x] < self.rank[root_y]:
+            self.parent[root_x] = root_y
+        elif self.rank[root_x] > self.rank[root_y]:
+            self.parent[root_y] = root_x
+        else:
+            self.parent[root_y] = root_x
+            self.rank[root_x] += 1  # Only grows if heights are equal
+        return True
+        
